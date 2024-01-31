b8f0d63bad2c95b70d43a379337ffc85d03d4ae3
manoharnelli7 <150885966+manoharnelli7@users.noreply.github.com>
2024-01-31 15:36:33 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 int main()
 {
-  printf("welcome to C stored in github repository");
-  printf("this line added using VS code")
-  // new line added
+  printf("updated hello.c");
 }
